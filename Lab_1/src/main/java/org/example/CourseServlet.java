1e8afc97d52f67f4f08310367c5b5d70f89d0188
ivashchenko-karina <ivashchenko_karina@knu.ua>
2026-02-23 16:36:42 +0200

    Add files via upload

---
+++ Lab_1/src/main/java/org/example/CourseServlet.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CourseServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<h1>Курс: Обробка даних в інформаційних технологіях.</h1>");
+        }
+    }
+}
